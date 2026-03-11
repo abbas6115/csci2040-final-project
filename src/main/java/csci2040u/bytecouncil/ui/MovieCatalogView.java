63482b7b790574eb5455674f76706ce58ee52776
shanj <shanjeofry@gmail.com>
2026-03-11 16:32:43 -0400

    added image functionality

--- src/main/java/csci2040u/bytecouncil/ui/MovieCatalogView.java
+++ src/main/java/csci2040u/bytecouncil/ui/MovieCatalogView.java
@@ -2,7 +2,9 @@ package csci2040u.bytecouncil.ui;
 
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
@@ -33,21 +35,42 @@ public class MovieCatalogView extends VerticalLayout {
         add(catalogGrid);
     }
 
-    private VerticalLayout createMovieCard(Movie movie) {
-        VerticalLayout movieCard = new VerticalLayout(
+    private HorizontalLayout createMovieCard(Movie movie) {
+        VerticalLayout movieDetails = new VerticalLayout(
                 new Paragraph("Name: " + valueOrNA(movie.getName())),
                 new Paragraph("Actors: " + valueOrNA(movie.getActors())),
                 new Paragraph("Genre: " + valueOrNA(movie.getGenre())),
                 new Paragraph("Ratings: " + valueOrNA(movie.getRatings())),
                 new Paragraph("Release Year: " + yearOrNA(movie.getReleaseYear()))
         );
-        movieCard.setSpacing(false);
-        movieCard.setPadding(false);
+        movieDetails.setSpacing(false);
+        movieDetails.setPadding(false);
+        movieDetails.getStyle().set("font-size", "0.9rem");
+        movieDetails.setFlexGrow(1);
+
+        //image
+        Image poster = new Image(movie.getPosterURL(),"");
+        poster.getStyle().set("object-fit", "contain");
+        poster.setWidth("400px");
+        poster.setHeight("240px");
+        poster.getStyle().set("background-image", "url('https://cdn-icons-png.flaticon.com/128/1665/1665664.png')");
+        poster.getStyle().set("object-fit", "cover");
+        poster.getStyle().set("border-radius", "4px");
+        poster.getStyle().set("background-repeat", "no-repeat");
+        poster.getStyle().set("background-position", "center");
+        poster.getStyle().set("background-size", "contain");
+
+        //make it horizontal layout so you can place the image beside the details
+        HorizontalLayout movieCard=new HorizontalLayout(movieDetails,poster);
+
+        movieCard.setSpacing(true);
         movieCard.getStyle().set("border", "1px solid #d3d3d3");
         movieCard.getStyle().set("border-radius", "8px");
         movieCard.getStyle().set("padding", "8px");
-        movieCard.getStyle().set("font-size", "0.9rem");
         movieCard.getStyle().set("min-height", "160px");
+        movieCard.setJustifyContentMode(JustifyContentMode.BETWEEN);
+        movieCard.setAlignItems(Alignment.CENTER);
+
         return movieCard;
     }
 
