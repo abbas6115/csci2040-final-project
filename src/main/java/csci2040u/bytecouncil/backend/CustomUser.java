3621eeab89367c22d8a1b26ccbd389db63f74817
Nabeel <04nabeel23@gmail.com>
2026-03-26 15:33:21 -0400

    created CustomUser.java

---
+++ src/main/java/csci2040u/bytecouncil/backend/CustomUser.java
@@ -0,0 +1,48 @@
+package csci2040u.bytecouncil.backend;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class CustomUser implements UserDetails {
+
+    private String username;
+    private String password;
+    private Queue<Movie> recentlyWatched = new LinkedList<>();
+
+    public CustomUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public void addToWatchlist(Movie movie) {
+        recentlyWatched.add(movie);
+    }
+
+    public Queue<Movie> getRecentlyWatched() { return recentlyWatched; }
+
+    @Override
+    public String getUsername() { return username; }
+
+    @Override
+    public String getPassword() { return password; }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() { return List.of(); }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+}
