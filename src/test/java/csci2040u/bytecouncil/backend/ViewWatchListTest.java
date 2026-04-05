f7537874dc1faeb2a40ffeaba82ce68fa4281569
Ethan J <ethanj4457@gmail.com>
2026-04-04 22:28:25 -0400

    Unit Test - View Watch List (Passing)

--- src/test/java/csci2040u/bytecouncil/backend/ViewWatchListTest.java
+++ src/test/java/csci2040u/bytecouncil/backend/ViewWatchListTest.java
@@ -27,7 +27,7 @@ public class ViewWatchListTest {
 
         //Test size of queue (2)
         assertEquals(2, result.size());
-        assertEquals(dune, result.get(0));
+        assertEquals(dune, result.get(1));
         assertEquals(transformers, result.get(0));
 
 
@@ -39,8 +39,6 @@ public class ViewWatchListTest {
     void WatchListEmpty() {
         //initialize components
         CustomUser user = new CustomUser("user1", "password", "user");
-        Movie dune = new Movie("Dune", "poster", "Timothee", "Sci-Fi", "8.0", 2021);
-        user.addToWatchlist(dune);
 
         //Test if watch list is empty
         assertTrue(user.getRecentlyWatched().isEmpty());
@@ -62,7 +60,7 @@ public class ViewWatchListTest {
 
         //Check size & first movie
         assertEquals(2, result.size());
-        assertEquals(dune, result.get(0));
+        assertEquals(dune, result.get(1));
 
         //remove movie
         user.removeMovieWatchList(transformers);
@@ -71,6 +69,6 @@ public class ViewWatchListTest {
 
         //Check size and first movie again
         assertEquals(1, result.size());
-        assertEquals(transformers, result.get(0));
+        assertEquals(dune, result.get(0));
     }
 }
